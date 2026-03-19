d3192d412eb2b9bf3acb84d8b668b1bb6ac675ea
NinoVilloutreix <nino.villoutreix@etu.unilim.fr>
2026-03-19 09:12:39 +0100

    carrousel maj

--- patterns/silksong_carrousel.php
+++ patterns/silksong_carrousel.php
@@ -9,8 +9,8 @@
 ?>
 <!-- wp:cover {"url":"https://mmi.unilim.fr/~villoutreix8/wordpress/wp-content/uploads/2026/03/Silksong-Bilewater-Header-1024x576.webp","id":86,"dimRatio":50,"overlayColor":"black","isUserOverlayColor":true,"minHeight":777,"minHeightUnit":"px","sizeSlug":"large","metadata":{"categories":["banner","call-to-action"],"patternName":"core/bold-sale-banner-with-geometric-background","name":"Bold sale banner with geometric background"},"align":"full","style":{"spacing":{"padding":{"top":"4vw","right":"4vw","bottom":"4vw","left":"4vw"},"margin":{"top":"0"}},"color":{"duotone":"unset"},"border":{"width":"2px"}},"layout":{"type":"default"}} -->
 <div class="wp-block-cover alignfull" style="border-width:2px;margin-top:0;padding-top:4vw;padding-right:4vw;padding-bottom:4vw;padding-left:4vw;min-height:777px"><img class="wp-block-cover__image-background wp-image-86 size-large" alt="" src="https://mmi.unilim.fr/~villoutreix8/wordpress/wp-content/uploads/2026/03/Silksong-Bilewater-Header-1024x576.webp" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-black-background-color has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:group {"style":{"spacing":{"blockGap":"0px"}},"layout":{"type":"default"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"59px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"1000","lineHeight":"0.9","letterSpacing":"-2px"}},"textColor":"white"} -->
-<p class="has-text-align-center has-white-color has-text-color" style="font-size:59px;font-style:normal;font-weight:1000;letter-spacing:-2px;line-height:0.9;text-transform:uppercase">Going somewhere muddy?</p>
+<div class="wp-block-group"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"59px","textTransform":"uppercase","fontStyle":"normal","fontWeight":"1000","lineHeight":"0.9","letterSpacing":"-2px"},"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"textColor":"white"} -->
+<p class="has-text-align-center has-white-color has-text-color" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);font-size:59px;font-style:normal;font-weight:1000;letter-spacing:-2px;line-height:0.9;text-transform:uppercase">Going somewhere muddy?</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:heading {"textAlign":"right"} -->
@@ -21,10 +21,14 @@
 <p class="has-text-align-right has-custom-blue-color has-text-color has-link-color has-medium-font-size" style="padding-top:var(--wp--preset--spacing--30);padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">Blue tool</p>
 <!-- /wp:paragraph -->
 
+<!-- wp:image {"id":148,"width":"288px","height":"auto","aspectRatio":"1.0382155669942286","sizeSlug":"full","linkDestination":"media","align":"right","style":{"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}}} -->
+<figure class="wp-block-image alignright size-full is-resized has-custom-border"><a href="https://mmi.unilim.fr/~villoutreix8/wordpress/wp-content/uploads/2026/03/Wreath_of_Purity.png"><img src="https://mmi.unilim.fr/~villoutreix8/wordpress/wp-content/uploads/2026/03/Wreath_of_Purity.png" alt="" class="wp-image-148" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;aspect-ratio:1.0382155669942286;width:288px;height:auto"/></a></figure>
+<!-- /wp:image -->
+
 <!-- wp:woocommerce/single-product {"productId":54} -->
 <div class="wp-block-woocommerce-single-product woocommerce"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"0","right":"0"},"blockGap":"0"},"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","orientation":"horizontal"}} -->
+<div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40","left":"0","right":"0"},"blockGap":"0"},"border":{"radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"left","orientation":"horizontal"}} -->
 <div class="wp-block-group" style="border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;padding-top:var(--wp--preset--spacing--40);padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0"><!-- wp:group {"fontSize":"medium","layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
 <div class="wp-block-group has-medium-font-size"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"medium"} -->
 <p class="has-medium-font-size">Fragile charm woven of reed and leafs.<br>Said to repels the Muckmaggots of Sinner’s road, Bilewater, and Putrified Ducks. Repairs when settling at benches</p>
@@ -32,7 +36,7 @@
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-fill","style":{"color":{"gradient":"linear-gradient(135deg,rgba(7,146,227,0) 0%,rgba(255,255,255,0.49) 52%,rgba(155,81,224,0) 100%)"},"border":{"width":"0px","style":"none","radius":{"topLeft":"0px","topRight":"0px","bottomLeft":"0px","bottomRight":"0px"}},"spacing":{"padding":{"left":"0","right":"0","top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"fontSize":"medium"} -->
-<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-background has-medium-font-size has-custom-font-size wp-element-button" style="border-style:none;border-width:0px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;background:linear-gradient(135deg,rgba(7,146,227,0) 0%,rgba(255,255,255,0.49) 52%,rgba(155,81,224,0) 100%);padding-top:var(--wp--preset--spacing--30);padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">BUY - 2500GEos</a></div>
+<div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill"><a class="wp-block-button__link has-background has-medium-font-size has-custom-font-size wp-element-button" href="https://mmi.unilim.fr/~villoutreix8/wordpress/product/wreath-of-purity/" style="border-style:none;border-width:0px;border-top-left-radius:0px;border-top-right-radius:0px;border-bottom-left-radius:0px;border-bottom-right-radius:0px;background:linear-gradient(135deg,rgba(7,146,227,0) 0%,rgba(255,255,255,0.49) 52%,rgba(155,81,224,0) 100%);padding-top:var(--wp--preset--spacing--30);padding-right:0;padding-bottom:var(--wp--preset--spacing--30);padding-left:0">BUY - 2500GEos</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group -->
@@ -43,11 +47,7 @@
 
 <!-- wp:separator {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
 <hr class="wp-block-separator has-alpha-channel-opacity" style="margin-top:0;margin-bottom:0"/>
-<!-- /wp:separator -->
-
-<!-- wp:woocommerce/product-image {"showSaleBadge":false,"isDescendentOfSingleProductBlock":true,"width":"300px","height":"279px","style":{"layout":{"selfStretch":"fit","flexSize":null},"dimensions":{"aspectRatio":"1"}},"fontSize":"small"} -->
-<!-- wp:woocommerce/product-sale-badge {"align":"right"} /-->
-<!-- /wp:woocommerce/product-image --></div>
+<!-- /wp:separator --></div>
 <!-- /wp:group --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
