40203ee2d78cb2f25ef5a8e5a61d3ffdc944a01b
NinoVilloutreix <nino.villoutreix@etu.unilim.fr>
2026-04-02 15:41:47 +0200

    map fixed

--- patterns/silksong_map.php
+++ patterns/silksong_map.php
@@ -9,14 +9,12 @@
 
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"silksong_title"} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60","left":"var:preset|spacing|40","right":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|80","margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--80);padding-top:var(--wp--preset--spacing--60);padding-right:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--40)"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"silksong_title"} -->
 <h1 class="wp-block-heading has-text-align-center has-silksong-title-font-size">Need a map ?</h1>
 <!-- /wp:heading -->
 
-<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--80)"><!-- wp:group {"style":{"spacing":{"blockGap":"13rem"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"13rem"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
 <div class="wp-block-group"><!-- wp:cover {"url":"https://mmi.unilim.fr/~villoutreix8/wordpress/wp-content/uploads/2026/03/Rectangle-13.png","id":96,"dimRatio":0,"style":{"spacing":{"padding":{"right":"5.7rem","left":"5.7rem"}}},"borderColor":"white"} -->
 <div class="wp-block-cover has-border-color has-white-border-color" style="padding-right:5.7rem;padding-left:5.7rem" id="117"><img class="wp-block-cover__image-background wp-image-96" alt="" src="https://mmi.unilim.fr/~villoutreix8/wordpress/wp-content/uploads/2026/03/Rectangle-13.png" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:heading {"level":1,"className":"wp-block-heading","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"silksong_title"} -->
 <h1 class="wp-block-heading has-white-color has-text-color has-link-color has-silksong-title-font-size"><a href="https://mmi.unilim.fr/~villoutreix8/wordpress/hallownest-maps/" data-type="page" data-id="120">Hallownest</a></h1>
@@ -28,7 +26,5 @@
 <h1 class="wp-block-heading has-white-color has-text-color has-link-color has-silksong-title-font-size"><a href="https://mmi.unilim.fr/~villoutreix8/wordpress/pharloom-maps/" data-type="page" data-id="117">Pharloom</a></h1>
 <!-- /wp:heading --></div></div>
 <!-- /wp:cover --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
